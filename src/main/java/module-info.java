761ac617a3ce13fd60da234d245635b22c96c8e4
magicghostvu <magicghost.vu@gmail.com>
2019-07-19 00:11:27 +0700

    #add rxjava 2 dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,6 @@
 module learing.rxjava {
     requires org.apache.logging.log4j;
     requires org.apache.logging.log4j.core;
+    requires io.reactivex.rxjava2;
+    requires org.reactivestreams;
 }
