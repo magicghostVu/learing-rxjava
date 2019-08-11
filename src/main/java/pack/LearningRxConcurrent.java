0edcfa366c590ca016f3924d7866a48259f51cb6
magicghostvu <magicghost.vu@gmail.com>
2019-08-12 00:04:50 +0700

    #update file learing concurrent

--- src/main/java/pack/LearningRxConcurrent.java
+++ src/main/java/pack/LearningRxConcurrent.java
@@ -1,24 +1,43 @@
 package pack;
 
+import io.reactivex.Observable;
+import io.reactivex.schedulers.Schedulers;
 import logging.LoggingService;
 import org.apache.logging.log4j.Logger;
+
+import java.util.concurrent.TimeUnit;
 
 public class LearningRxConcurrent {
     private static Logger logger = LoggingService.getInstance().getLogger();
 
-    public static void main(String[] args) {
-        logger.info("Main run");
+
+    static void useSubcribeOn() throws Exception {
+        var sc = Schedulers.single();
+        var g = Observable.just("phuvh", "vint", "quyvv")
+                .subscribeOn(sc)
+                .subscribe(str -> {
+                    logger.info("str is {}", str);
+                });
 
 
+        //Observable.inon
 
-        Runnable r= ()->{
-          logger.info("Run");
-        };
+        Observable.interval(1000, TimeUnit.MILLISECONDS)
+                .take(10)
+                .map(i -> {
+                    logger.info("on map {}", i);
+                    return String.valueOf(i);
+                })
+                .subscribeOn(sc)
+                .subscribe(i -> {
+                    logger.info("i is {}", i);
+                });
 
-        new Thread(r).start();
-        new Thread(r).start();
+        //logger.info("g is {}", g.isDisposed());
+        Thread.sleep(5000);
+    }
 
-
-
+    public static void main(String[] args) throws Exception {
+        useSubcribeOn();
     }
 }
