cec2d3d189b87fa2988b1660c1fe444c3b9589c8
magicghostvu <magicghost.vu@gmail.com>
2019-08-13 00:32:01 +0700

    #add subscribe on

--- src/main/java/pack/LearningRxConcurrent.java
+++ src/main/java/pack/LearningRxConcurrent.java
@@ -22,7 +22,8 @@ public class LearningRxConcurrent {
 
         //Observable.inon
 
-        Observable.interval(1000, TimeUnit.MILLISECONDS)
+        //khi sử dụng interval nếu muốn nó chạy trên một schedule chỉ định thì phải thêm tham số vào cuối
+        Observable.interval(1000, TimeUnit.MILLISECONDS, sc)
                 .take(10)
                 .map(i -> {
                     logger.info("on map {}", i);
@@ -37,7 +38,34 @@ public class LearningRxConcurrent {
         Thread.sleep(5000);
     }
 
+
+    private static void useObserverOn() throws InterruptedException {
+
+        var sc1 = Schedulers.newThread();
+        var sc2 = Schedulers.single();
+
+        var y = Observable.interval(1000, TimeUnit.MILLISECONDS, sc1)
+                .map(i -> {
+
+                    logger.info("map run i is {}", i);
+
+                    return i;
+                }).take(10)
+                .observeOn(sc2)
+                .reduce(0L, Long::sum)
+                .subscribe(r -> {
+                    logger.info("result on {}", r);
+                });
+
+        logger.info("y is {}", y);
+
+        Thread.sleep(10000);
+    }
+
+
     public static void main(String[] args) throws Exception {
-        useSubcribeOn();
+        useObserverOn();
+
+
     }
 }
